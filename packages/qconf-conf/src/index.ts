f62ed7c2c99f0dd8866f62b2ea7d717b26d942bc
jarvis <jiashunming@outlook.com>
2019-04-18 17:43:25 +0800

    feat: add qconf dependencies

--- packages/qconf-conf/src/index.ts
+++ packages/qconf-conf/src/index.ts
@@ -1,3 +1,26 @@
-export default function main () {
-  return 'Hello World.'
+import ConfIntl from '@blued-core/conf-intl'
+import { Qconf } from '@blued-core/qconf'
+
+export class QconfConf implements ConfIntl {
+  private qconf: Qconf
+
+  constructor(public configs: Record<string, any>) {
+    this.qconf = new Qconf(configs)
+  }
+
+  get(key: string) {
+    return this.qconf.getConf(key)
+  }
+}
+
+export class QconfHost implements ConfIntl {
+  private qconf: Qconf
+
+  constructor(public configs: Record<string, any>) {
+    this.qconf = new Qconf(configs)
+  }
+
+  get(key: string) {
+    return this.qconf.getHost(key)
+  }
 }
