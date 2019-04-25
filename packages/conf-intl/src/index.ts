0af6fe1fa144147b085e3442ce7f8ae9acf2f21e
jarvis <jiashunming@outlook.com>
2019-04-25 11:19:23 +0800

    feat: 添加新的接口导出

--- packages/conf-intl/src/index.ts
+++ packages/conf-intl/src/index.ts
@@ -3,3 +3,19 @@ export default interface ConfIntl<T extends any = any, ConfigItem extends any = 
 
   get(key: string): T
 }
+
+export type NormalConfInstance = string | null
+
+export interface HostPortConfInstance {
+  host: string
+  port: string
+}
+
+export interface MysqlConfInstance {
+  masterHost: string
+  slaveHost: string[]
+  username: string
+  password: string
+  database: any
+  modelPath: any
+}
