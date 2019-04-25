6cb62cc00d8481e9fd4fe97ede53a1972e26b4b4
jarvis <jiashunming@outlook.com>
2019-04-25 13:57:12 +0800

    feat: 更新redis-conf实现方式

--- packages/redis-conf/src/index.ts
+++ packages/redis-conf/src/index.ts
@@ -1,11 +1,11 @@
-import ConfIntl, { HostPortConfInstance } from '@blued-core/conf-intl'
+import ConfIntl from '@blued-core/conf-intl'
 import { Qconf } from '@blued-core/qconf'
 
 export type RedisConfItem = {
   qconf: string
 } | string
 
-export class RedisConf implements ConfIntl<HostPortConfInstance, RedisConfItem> {
+export class RedisConf implements ConfIntl {
   private qconf: Qconf
 
   constructor(public configs: Record<string, RedisConfItem>) {
