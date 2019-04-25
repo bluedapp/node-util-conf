2c70595ac2e08e8af57c7f80121a4eeb8a3c1b13
jarvis <jiashunming@outlook.com>
2019-04-25 13:49:57 +0800

    refactor: 添加返回值的约束

--- packages/conf-intl/src/index.ts
+++ packages/conf-intl/src/index.ts
@@ -1,21 +1,5 @@
-export default interface ConfIntl<T extends any = any, ConfigItem extends any = any> {
-  configs: Record<string, ConfigItem>
+export default interface ConfIntl<T extends any = any> {
+  configs: Record<string, any>
 
   get(key: string): T
-}
-
-export type NormalConfInstance = string | null
-
-export interface HostPortConfInstance {
-  host: string
-  port: string
-}
-
-export interface MysqlConfInstance {
-  masterHost: string
-  slaveHost: string[]
-  username: string
-  password: string
-  database: any
-  modelPath: any
 }
